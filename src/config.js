9737efe784f6b982f8da43fccb3880f13857bc3c
Fabien Sanchez <fzed51+git@gmail.com>
2019-04-03 16:44:32 +0200

    le module ne retourne plus de config static mais une config modulable

--- src/config.js
+++ src/config.js
@@ -1,31 +1,69 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-const config = {
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
-  },
-  target: "web",
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/i,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          plugins: [
-            "@babel/plugin-syntax-dynamic-import",
-            "@babel/plugin-syntax-import-meta",
-            ["@babel/plugin-proposal-class-properties", { loose: false }],
-            "@babel/plugin-proposal-json-strings"
-          ],
-          presets: [
-            ["@babel/preset-env", { modules: false }],
-            "@babel/preset-react"
-          ]
+/**
+ * génère une config pour webpack
+ * @param {{useReact?: boolean,useTypescript?: boolean,htmlWebpackPlugin?: boolean|{title?: string, template?: string}}} options
+ */
+const configGenerator = options => {
+  const optionsBase = {
+    useReact: true,
+    useTypescript: true,
+    htmlWebpackPlugin: true
+  };
+  options = { ...optionsBase, ...options };
+  const extensions = [".js", ".json"];
+  const presets = [["@babel/preset-env", { modules: false }]];
+  if (options.useReact) {
+    extensions = [...extensions, ".jsx"];
+    presets = [...presets, "@babel/preset-react"];
+  }
+  const rules = [
+    {
+      test: /\.jsx?$/i,
+      exclude: /node_modules/,
+      loader: "babel-loader",
+      options: {
+        plugins: [
+          "@babel/plugin-syntax-dynamic-import",
+          "@babel/plugin-syntax-import-meta",
+          ["@babel/plugin-proposal-class-properties", { loose: false }],
+          "@babel/plugin-proposal-json-strings"
+        ],
+        presets
+      }
+    },
+    {
+      test: /\.s?css$/i,
+      use: [
+        { loader: "style-loader" },
+        { loader: "css-loader" },
+        { loader: "sass-loader" }
+      ]
+    },
+    {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      use: [
+        {
+          loader: "url-loader",
+          options: { name: "img/[hash].[ext]" }
         }
-      },
+      ]
+    },
+    {
+      test: /\.(ttf|woff2?|eof|eot)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: { name: "font/[hash].[ext]" }
+        }
+      ]
+    }
+  ];
+  if (options.useTypescript) {
+    extensions = [...extensions, ".ts"];
+    rules = [
+      ...rules,
       {
         test: /\.tsx?$/i,
         exclude: /node_modules/,
@@ -37,50 +75,38 @@ const config = {
             ["@babel/plugin-proposal-class-properties", { loose: false }],
             "@babel/plugin-proposal-json-strings"
           ],
-          presets: [
-            ["@babel/preset-env", { modules: false }],
-            "@babel/preset-react",
-            "@babel/preset-typescript"
-          ]
+          presets: [...presets, "@babel/preset-typescript"]
         }
-      },
-      {
-        test: /\.s?css$/i,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "sass-loader" }
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: { name: "img/[hash].[ext]" }
-          }
-        ]
-      },
-      {
-        test: /\.(ttf|woff2?|eof|eot)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: { name: "font/[hash].[ext]" }
-          }
-        ]
       }
-    ]
-  },
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      chunks: "all",
-      minSize: 0,
-      maxSize: 100000
+    ];
+  }
+  if (options.useReact && options.useTypescript) {
+    extensions = [...extensions, ".tsx"];
+  }
+  const plugins = [new CleanWebpackPlugin()];
+  if (!!options.htmlWebpackPlugin) {
+    if (options.htmlWebpackPlugin === true) {
+      plugins = [...plugins, new HtmlWebpackPlugin()];
+    } else {
+      plugins = [...plugins, new HtmlWebpackPlugin(options.htmlWebpackPlugin)];
     }
   }
+
+  return {
+    resolve: { extensions },
+    target: "web",
+    devtool: "source-map",
+    module: { rules },
+    plugins,
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+        minSize: 0,
+        maxSize: 100000
+      }
+    }
+  };
 };
 
 module.exports = config;
